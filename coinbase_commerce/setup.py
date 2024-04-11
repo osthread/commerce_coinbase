2681ce76e92f483350c0f4309a592e09e59a708c
Maxie <76827213+MaxieDev@users.noreply.github.com>
2024-04-11 01:30:02 -0500

    Update setup.py

--- coinbase_commerce/setup.py
+++ coinbase_commerce/setup.py
@@ -15,7 +15,7 @@ setup(
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: GPL-3.0 license',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.9',
